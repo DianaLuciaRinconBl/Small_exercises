c032a88569b492f1a2bf7050f8ccb6b65bd9c60f
Diana <dianarinconbl@gmail.com>
2019-03-12 18:49:32 +1100

    ex 9

---
+++ Easy1/ex_9.rb
@@ -0,0 +1,16 @@
+# Write a method that takes one argument, a positive integer,
+# and returns the sum of its digits.
+
+def sum(number)
+  number.digits.sum
+end
+
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
+
+# LaunchSchool solution:
+#
+# def sum(number)
+#   number.to_s.chars.map(&:to_i).reduce(:+)
+# end
